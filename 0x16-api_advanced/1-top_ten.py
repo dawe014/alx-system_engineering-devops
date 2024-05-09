7066700f677c86784b340cd2c26bf9b41f67c5ff
dawe014 <dawittamiru014@gmail.com>
2024-05-09 20:21:51 +0300

    API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,21 @@
+#!/usr/bin/python3
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-    """
-    Print the titles of the first 10 hot posts for a given subreddit.
-    If the subreddit is invalid, print None.
-    """
-    user_agent = {'User-Agent': 'Lizzie'}
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json?limit=10"
-    response = requests.get(url, headers=user_agent, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        for post in data.get('data', {}).get('children', []):
-            print(post.get('data', {}).get('title'))
-    elif response.status_code == 404:
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
-    else:
-        print("Error:", response.status_code)
-
-if __name__ == "__main__":
-    subreddit = input("Enter the subreddit: ")
-    top_ten(subreddit)
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""
-    Recurse it!
-"""
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """
-        Return a redirect to search results.
-    """
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "after": after,
