06c7867bb6bae69cf5b4642b631be6e6bec14d74
dawe014 <dawittamiru014@gmail.com>
2024-05-09 20:18:38 +0300

    API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,23 @@
-#!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints the titles of
-the first 10 hot posts listed for a given subreddit.
-"""
 import requests
-from sys import argv
 
 def top_ten(subreddit):
     """
     Print the titles of the first 10 hot posts for a given subreddit.
+    If the subreddit is invalid, print None.
     """
     user_agent = {'User-Agent': 'Lizzie'}
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json?limit=10"
-    response = requests.get(url, headers=user_agent)
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
         for post in data.get('data', {}).get('children', []):
             print(post.get('data', {}).get('title'))
+    elif response.status_code == 404:
+        print("None")
     else:
         print("Error:", response.status_code)
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: python script.py <subreddit>")
-    else:
-        top_ten(argv[1])
+    subreddit = input("Enter the subreddit: ")
+    top_ten(subreddit)
