0ff7ce8083bc38373b9581c07bd6212bc8ecef54
dawe014 <dawittamiru014@gmail.com>
2024-05-24 21:52:14 +0300

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,28 @@
-#!/usr/bin/python3
-"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
-
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+def recurse(subreddit, hot_list=[], after=None):
+    """Recursively return a list of titles of all hot articles for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        return None
+    params = {'after': after} if after else {}
+    
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json().get("data", {})
+        children = data.get("children", [])
+        hot_list.extend([child["data"]["title"] for child in children])
+        after = data.get("after")
+        
+        if after:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+    
+    return None if response.status_code == 404 else hot_list
 
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+# Example usage:
+# print(recurse('python'))  # Should return a list of titles of hot articles in r/python
+# print(recurse('thissubredditdoesnotexist'))  # Should return None
