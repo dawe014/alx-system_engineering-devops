213a331da666334906546715ae5f4451ab94c652
dawe014 <dawittamiru014@gmail.com>
2024-05-24 21:54:41 +0300

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,38 @@
+#!/usr/bin/python3
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
-def recurse(subreddit, hot_list=[], after=None):
-    """Recursively return a list of titles of all hot articles for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+def recurse(subreddit, hot_list=None, after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    if hot_list is None:
+        hot_list = []
+
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {'after': after} if after else {}
-    
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
     response = requests.get(url, headers=headers, params=params, allow_redirects=False)
     
-    if response.status_code == 200:
-        data = response.json().get("data", {})
-        children = data.get("children", [])
-        hot_list.extend([child["data"]["title"] for child in children])
-        after = data.get("after")
-        
-        if after:
-            return recurse(subreddit, hot_list, after)
-        else:
-            return hot_list
-    
-    return None if response.status_code == 404 else hot_list
+    if response.status_code == 404:
+        return None
 
-# Example usage:
-# print(recurse('python'))  # Should return a list of titles of hot articles in r/python
-# print(recurse('thissubredditdoesnotexist'))  # Should return None
+    if response.status_code != 200:
+        return hot_list
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist", 0)
+
+    for child in results.get("children", []):
+        hot_list.append(child.get("data").get("title"))
+
+    if after:
+        return recurse(subreddit, hot_list, after, count)
+    
+    return hot_list
