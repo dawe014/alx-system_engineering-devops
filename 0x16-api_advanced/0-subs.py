00c3f0f89d8eb4f410258fea62e476e1577e3069
dawe014 <dawittamiru014@gmail.com>
2024-05-24 21:50:09 +0300

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code == 404:
-        results = response.json().get("data")
-        if results and "subscribers" in results:
-            return results.get("subscribers")
-    
-    # Return 0 if the subreddit does not exist or other issues
-    return 0
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
